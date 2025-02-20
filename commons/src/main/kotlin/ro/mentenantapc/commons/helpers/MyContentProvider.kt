23b4428e780439d5acee0b0ec21ef4c420dac026
Biggy <165229481+Biggyalecs@users.noreply.github.com>
2025-02-20 15:30:30 +0200

    initial Release

--- commons/src/main/kotlin/ro/mentenantapc/commons/helpers/MyContentProvider.kt
+++ commons/src/main/kotlin/ro/mentenantapc/commons/helpers/MyContentProvider.kt
@@ -5,11 +5,12 @@ import android.net.Uri
 object MyContentProvider {
     private const val AUTHORITY = "ro.mentenantapc.android.provider"
     val MY_CONTENT_URI: Uri = Uri.parse("content://$AUTHORITY/settings")
-
+    const val SHARED_THEME_ACTIVATED = "ro.mentenantapc.commons.SHARED_THEME_ACTIVATED"
+    const val SHARED_THEME_UPDATED = "ro.mentenantapc.commons.SHARED_THEME_UPDATED"
     const val ACTION_GLOBAL_CONFIG_UPDATED = "ro.mentenantapc.android.GLOBAL_CONFIG_UPDATED"
     const val PERMISSION_WRITE_GLOBAL_SETTINGS = "ro.mentenantapc.android.permission.WRITE_GLOBAL_SETTINGS"
 
-    const val COL_ID = "_id"    // used in Fossify Thank You
+    const val COL_ID = "_id"    // used in MentenantaPc Thank You
     const val COL_THEME_TYPE = "theme_type"
     const val COL_TEXT_COLOR = "text_color"
     const val COL_BACKGROUND_COLOR = "background_color"
